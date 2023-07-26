21895b268083e4732409e45ae58a19b6b17e659c
Abdo <abdouumostafa1@gmail.com>
2023-07-26 17:00:23 +0300

    initial commit

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer>
-    <h5 className="bg-black text-center p-6 text-white font-bold">{new Date().getFullYear} All Rights Reserved - MYM  &copy;</h5>
+    <h5 className="bg-black text-center p-6 text-white font-bold">2023 All Rights Reserved - MYM  &copy;</h5>
   </footer>
   )
 }
